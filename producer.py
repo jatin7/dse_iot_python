d9a65c3b46e52c13643f885c8eee4cae5b472f43
Your Name <you@example.com>
2019-03-29 13:41:39 +0000

    converting to json

--- producer.py
+++ producer.py
@@ -3,6 +3,7 @@ from kafka import KafkaProducer
 from random import randint
 from ConfigParser import ConfigParser
 import time
+import json
 
 config = ConfigParser()
 config.read('iot.ini')
@@ -13,13 +14,19 @@ sensorCount = config.get('IOT','devices')
 sensorTypes = config.get('IOT','types').split(',')
 
 while 1:
+   time.sleep(0.5)
    current = time.localtime()
    bucket = str(current.tm_year) + str(current.tm_mon) + str(current.tm_mday) + str(current.tm_hour) + str(current.tm_min)
    ts = time.strftime('%Y-%m-%dT%H:%M:%S', current)
    sensor = randint(1,int(sensorCount))
    for t in sensorTypes:
       reading = randint(1,100)
-      msg = bucket + "," + str(ts) + "," + str(sensor) + "," + t + "," + str(reading)
-      print msg
-      #producer.send("dse-iot", msg.strip())
-      #producer.flush()
+      data = {}
+      data['bucket'] = bucket
+      data['ts'] = ts 
+      data['sensor'] = sensor 
+      data['reading'] = reading
+      data['type'] = t
+      print json.dumps(data)
+      producer.send("dse-iot", json.dumps(data))
+      producer.flush()
